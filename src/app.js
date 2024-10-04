f2c1b91e4f7af2dd8b2a445263f635a044e2109d
Nepta <63590772+Neptali-Ricardo@users.noreply.github.com>
2024-10-04 18:34:46 +0000

    Proyecto Domain Name Generator completado

--- src/app.js
+++ src/app.js
@@ -9,3 +9,33 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let tlds = [".com", ".net", ".us", ".io"];
+
+const getRandomElement = array => {
+  if (!Array.isArray(array) || array.length === 0) return "";
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const domainGenerator = () => {
+  return `${getRandomElement(pronoun)}${getRandomElement(
+    adj
+  )}${getRandomElement(noun)}<strong>${getRandomElement(tlds)}</strong>`;
+};
+
+const domainText = () => {
+  const domainElement = document.getElementById("domain-generator__text");
+  if (domainElement) {
+    domainElement.innerHTML = domainGenerator();
+  }
+};
+
+const initDomainGenerator = () => {
+  domainText();
+  setInterval(domainText, 2500);
+};
+
+initDomainGenerator();
