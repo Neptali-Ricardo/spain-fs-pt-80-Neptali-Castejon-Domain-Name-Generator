70cc57b04579fdb66eb612f495b6c459b42a9ad7
Nepta <63590772+Neptali-Ricardo@users.noreply.github.com>
2024-10-05 07:56:50 +0000

    Implementa mejoras en la generación de dominios, incluyendo gestión de clases de iconos y alertas

--- src/app.js
+++ src/app.js
@@ -13,7 +13,23 @@ window.onload = function() {
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let tlds = [".com", ".net", ".us", ".io"];
+let tlds = [".com", ".net", ".es", ".info", ".org"];
+let alertClasses = [
+  "alert-warning",
+  "alert-success",
+  "alert-danger",
+  "alert-info"
+];
+let iconsClasses = [
+  "fa-face-smile",
+  "fa-face-surprise",
+  "fa-face-flushed",
+  "fa-face-dizzy",
+  "fa-face-grin-hearts",
+  "fa-face-frown",
+  "fa-face-smile-wink",
+  "fa-face-grin-tongue-squint"
+];
 
 const getRandomElement = array => {
   if (!Array.isArray(array) || array.length === 0) return "";
@@ -21,21 +37,45 @@ const getRandomElement = array => {
 };
 
 const domainGenerator = () => {
-  return `${getRandomElement(pronoun)}${getRandomElement(
+  return `www.<strong>${getRandomElement(pronoun)}${getRandomElement(
     adj
-  )}${getRandomElement(noun)}<strong>${getRandomElement(tlds)}</strong>`;
+  )}${getRandomElement(noun)}</strong>${getRandomElement(tlds)}`;
 };
 
-const domainText = () => {
+const updateDomainText = () => {
   const domainElement = document.getElementById("domain-generator__text");
   if (domainElement) {
     domainElement.innerHTML = domainGenerator();
   }
 };
 
+const updateAlertClass = () => {
+  const containerElement = document.getElementById("domain-generator");
+  if (containerElement) {
+    // Remueve las clases de alerta anteriores y agrega una nueva aleatoria
+    containerElement.classList.remove(...alertClasses);
+    containerElement.classList.add("alert", getRandomElement(alertClasses));
+  }
+};
+
+const updateDomainIcon = () => {
+  const iconElement = document.getElementById("domain-generator__icon");
+  if (iconElement) {
+    // Remueve las clases de íconos anteriores y agrega una nueva aleatoria
+    iconElement.classList.remove(...iconsClasses);
+    iconElement.classList.add("fa-regular", getRandomElement(iconsClasses));
+  }
+};
+
 const initDomainGenerator = () => {
-  domainText();
-  setInterval(domainText, 2500);
+  const updateAll = () => {
+    updateDomainText();
+    updateAlertClass();
+    updateDomainIcon();
+  };
+
+  updateAll(); // Llama a la función de actualización inicial
+  setInterval(updateAll, 2500); // Actualiza cada 2.5 segundos
 };
 
 initDomainGenerator();
